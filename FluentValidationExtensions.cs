b8f6109e4e5e104f171bcd8183605f15d3e098a0
saminHma <samin.h.m.a@gmail.com>
2023-12-29 15:51:19 +0330

    remove extra comment and code

--- FluentValidationExtensions.cs
+++ FluentValidationExtensions.cs
@@ -5,30 +5,11 @@ namespace ValidationForMassTransit;
 /// <summary>
 /// Extension methods for injecting functionality from the <see cref="FluentValidation"/> library into a <see cref="MassTransit"/> pipeline.
 /// </summary>
-public static class FluentValidationExtensions
-{
-    /// <summary>
-    /// Registers the necessary filters to perform validation on message sent through the pipeline.
-    /// </summary>
-    /// <param name="configurator">A MassTransit endpoint configurator.</param>
-    /// <param name="context">The message handling context, implementing <see cref="IConfigurationServiceProvider"/>.</param>
-    /// <example>
-    /// services.AddMassTransit(busConfigurator =>
-    ///     {
-    ///         busConfigurator.AddConsumer<MyHandler>();
-    ///         busConfigurator.UsingRabbitMq((busContext, rabbitMQConfigurator) =>
-    ///         {
-    ///             rabbitMQConfigurator.ReceiveEndpoint(AssemblyName, endpointConfigurator =>
-    ///             {
-    ///                 endpointConfigurator.UseFluentValidationForMassTransit(busContext);
-    ///                 endpointConfigurator.ConfigureConsumer<MyHandler>(busContext);
-    ///             });
-    ///         });
-    ///     });
-    /// </example>
-    public static IEndpointConfigurator UseFluentValidationForMassTransit(this IEndpointConfigurator configurator, IBusRegistrationContext context)
-    {
-        configurator.UseConsumeFilter(typeof(FluentValidationFilter<>), context);
-        return configurator;
-    }
-}
+//public static class FluentValidationExtensions
+//{
+//    public static IEndpointConfigurator UseFluentValidationForMassTransit(this IEndpointConfigurator configurator, IBusRegistrationContext context)
+//    {
+//        configurator.UseConsumeFilter(typeof(FluentValidationFilter<>), context);
+//        return configurator;
+//    }
+//}
